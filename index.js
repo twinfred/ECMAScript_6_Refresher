a359e3876f10d23cd494e3769fb23f518658171a
Tim Winfred <twinfred@gmail.com>
2020-10-13 21:06:47 -0700

    Finished lesson 3 and half of lesson 4

--- index.js
+++ index.js
@@ -83,16 +83,155 @@
 
 // Sets
 
-let books = new Set();
-books.add('Pride and Prejudice');
-books.add('War and Peace').add('Oliver Twist');
+// let books = new Set();
+// books.add('Pride and Prejudice');
+// books.add('War and Peace').add('Oliver Twist');
 
-console.log(books);
-console.log(books.size);
+// console.log(books);
+// console.log(books.size);
 
-books.delete('Oliver Twist');
-books.forEach(function(item) {
-  console.log(item);
-})
+// books.delete('Oliver Twist');
+// books.forEach(function(item) {
+//   console.log(item);
+// })
 
-console.log('has Oliver Twist', books.has('Oliver Twist'));
+// console.log('has Oliver Twist', books.has('Oliver Twist'));
+
+
+
+// Spread Operator
+
+// let cats = ['Biscuit', 'Henry', 'Margaret'];
+// let dogs = ['Noodle', 'Jojo', 'Shadow'];
+// let animals = ['Bamboo', 'Hector', 'Alphabet', ...cats, ...dogs];
+
+// console.log(animals);
+
+
+
+// Destructuring Arrays
+
+// let [first, second, third, fourth, fifth] = ['Chicago', 'Boston', 'San Jose', 'Portland', 'Seattle'];
+// console.log(first);
+// console.log(third);
+
+
+
+// Arrary Includes
+
+// let cities = ['Chicago', 'Boston', 'San Jose', 'Portland', 'Seattle'];
+
+// console.log(cities.includes('Seattle'));
+// console.log(cities.includes('Los Angeles'));
+
+
+
+// Object Literals Enhancement
+
+// function pokemon(name, gender) {
+//   return {
+//     name,
+//     gender,
+//     sayName: function() {
+//       console.log(`${name}, ${name}!!`);
+//     }
+//   }
+// }
+
+// let Pikachu = pokemon('Pikachu', 'male');
+// console.log(Pikachu.gender);
+
+
+
+// Objects with Spread Operator
+
+// const daytime = {
+//   breakfast: 'oatmeal',
+//   lunch: 'sandwich',
+// }
+
+// const nighttime = 'steak';
+
+// const meals = {
+//   ...daytime,
+//   nighttime,
+// };
+
+// console.log(meals);
+
+
+
+// Destructuring Objects
+
+// const { name, price, description, ingredients } = {
+//   name: 'Turkey',
+//   price: 6.5,
+//   description: 'Turkey on white bread',
+//   ingredients: [
+//     'bread',
+//     'oven-baked turkey',
+//     'mustard',
+//     'mayo',
+//     'cheese',
+//     'lettuce',
+//   ],
+// };
+
+// console.log(name);
+// console.log(description);
+
+// const hamSandwich = {
+//   name: 'Ham',
+//   price: 6.5,
+//   description: 'A delcious ham sandwich on white bread',
+//   ingredients: [
+//     'bread',
+//     'honey-baked ham',
+//     'mustard',
+//     'mayo',
+//     'cheese',
+//     'lettuce',
+//   ],
+// };
+
+// function promotion({ name, description }) {
+//   return `Try the ${name} sandwich! ${description}!`;
+// }
+
+// console.log(promotion(hamSandwich));
+
+
+
+// For / Of
+
+for (let letter of 'Timothy') {
+  console.log(letter);
+}
+
+let fruits = ['banana', 'apple', 'cherry'];
+
+for (let fruit of fruits) {
+  console.log(fruit);
+}
+
+let sandwiches = new Map();
+
+sandwiches.set('turkey', 'A delcious turkey sandwich on white bread');
+sandwiches.set('ham', 'A delcious ham sandwich on wheat bread');
+sandwiches.set('chicken', 'A delcious chicken sandwich on whole grain bread');
+
+for (let sandwich of sandwiches) {
+  console.log(sandwich);
+}
+
+for (let sandwich of sandwiches.keys()) {
+  console.log(sandwich);
+}
+
+for (let sandwich of sandwiches.values()) {
+  console.log(sandwich);
+}
+
+for (let sandwich of sandwiches.entries()) {
+  console.log(sandwich);
+}
